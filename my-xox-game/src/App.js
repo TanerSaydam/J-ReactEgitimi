2804349dac9a91cef859f1063ed217a737010606
Taner Saydam <tanersaydam@gmail.com>
2023-06-20 15:19:06 +0300

    .

--- my-xox-game/src/App.js
+++ my-xox-game/src/App.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import './MyStyle.css'
 
 function App(){
     const [game,setGame] =  useState([
@@ -11,19 +12,29 @@ function App(){
         "",
         "",
         "",
-    ])
-    const style = {
-        height: "100px",
-        backgroundColor: "red",
-        border: "1px solid white"
+    ]);
+
+    function setDivMark(i){
+        const newGame = game.map((val,index)=> {
+            if(index == i) return "X";
+            else return val
+        });
+
+        setGame(newGame);
     }
+   
     return(
         <div className="container" style={{marginTop: "20px"}}>
             <div className="row">
                 <div className="col-8">
                     <div className="row">
-                        <div className="col-4" style={style}>
-                        </div>                        
+                       {game.map((val,index)=> 
+                            <div key={index} 
+                                 className="col-4 game"
+                                 onClick={()=> setDivMark(index)}>
+                                {val}
+                            </div> 
+                       )}                   
                     </div>
                 </div>
                 <div className="col-4">
