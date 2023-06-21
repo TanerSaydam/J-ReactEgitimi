f4f122aa203b721276d020e0de6b8b34d7b165a9
Taner Saydam <tanersaydam@gmail.com>
2023-06-21 14:43:56 +0300

    .

--- Backend/index.js
+++ Backend/index.js
@@ -1,32 +1,27 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
 app.use(express.json());
 
-let customers = [    
-    {
-        name: "Taner Saydam",
-        profession: "Yazılım",
-        age: 33
-    }
-    ,
-    {
-        name: "Taner Saydam",
-        profession: "Yazılım",
-        age: 33
-    }
-    ,
-    {
-        name: "Taner Saydam",
-        profession: "Yazılım",
-        age: 33
-    }
-]
-
 app.get("/", (req, res)=> {
-    res.json(customers)
+    const payload = {
+        name: "Taner Saydam",
+        email: "tanersaydam@gmail.com"
+    }
+
+    const options = {
+        expiresIn: "30d"
+    }
+
+    const secretKey = "my secret key my secret key 1324546as."
+
+
+    let token = jwt.sign(payload,secretKey,options);
+
+    res.json({accessToken: token})
 });
 
 app.listen(5000);
